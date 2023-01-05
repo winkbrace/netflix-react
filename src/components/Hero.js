29745b250edb811cfe7c42717e6c31dcb26917c8
Bas de Ruiter <bas@basbv.it>
2023-01-05 13:14:42 +0100

    Add buttons to the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,16 @@
 import './Hero.css';
+import Button from "./button/Button";
 
 const Hero = (props) => {
-    return(
+    return (
         <div className="hero-container">
             <img src={props.background} alt="" className="hero"/>
             <div className="vignette">
                 <img src={props.logo} alt={props.title}/>
                 <p>{props.synopsis}</p>
                 <div className="button-container">
-                    <button className="color-primary">Afspelen</button>
-                    <button className="color-secondary">Meer informatie</button>
+                    <Button type="primary" icon="play" text="Afspelen"/>
+                    <Button type="secondary" icon="info" text="Meer informatie"/>
                 </div>
             </div>
         </div>
